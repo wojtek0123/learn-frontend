9902c50ee40e574023c2f7e1a12174c5f57e8e6a
WojtekP <wojtekpietraszuk@gmail.com>
2023-03-11 18:45:13 +0100

    Add route for page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { LearnAngularComponent } from './learn-angular/learn-angular.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'learn-angular',
+    component: LearnAngularComponent,
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
   },
 ];
 
