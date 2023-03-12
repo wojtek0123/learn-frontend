4e51436cae9813a21737a18414ab1e670b5f0918
WojtekP <wojtekpietraszuk@gmail.com>
2023-03-12 12:56:07 +0100

    Create basic look of accordion

---
+++ src/app/shared/ui/accordion/accordion.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-accordion',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './accordion.component.html',
+  styleUrls: ['./accordion.component.css'],
+})
+export class AccordionComponent {
+  @Input() question!: string;
+  @Input() answer!: string;
+  show = false;
+  onShow() {
+    this.show = !this.show;
+  }
+}
