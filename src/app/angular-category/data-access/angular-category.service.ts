b24806dcdb094e441956991fab7dd774f37675da
WojtekP <wojtekpietraszuk@gmail.com>
2023-03-14 14:05:58 +0100

    Generate environments

--- src/app/angular-category/data-access/angular-category.service.ts
+++ src/app/angular-category/data-access/angular-category.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import request from 'graphql-request';
 import { from } from 'rxjs';
 import { Record } from 'src/app/models/record.model';
-import { environment } from 'src/environments/environments';
 import { getRecords } from '../../shared/data-access/record.query';
+import { environment } from '../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  graphCmsEndpoint:
+    'https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clf4danvs4n6h01uj5aj8c9pp/master',
+};
