75b79c6626c600a10913b354b5115547f0828d70
WojtekP <wojtekpietraszuk@gmail.com>
2023-03-14 13:55:52 +0100

    Create a navigation component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgSwitch, NgSwitchDefault, NgSwitchCase } from '@angular/common';
+import { NavigationComponent } from './shared/ui/navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [NgSwitch, NgSwitchDefault, NgSwitchCase, RouterOutlet],
+  imports: [
+    NgSwitch,
+    NgSwitchDefault,
+    NgSwitchCase,
+    RouterOutlet,
+    NavigationComponent,
+  ],
 })
 export class AppComponent {
   title = 'learn-frontend';
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationLinksComponent } from './feature/navigation-links/navigation-links.component';
 import { AccordionComponent } from '../shared/ui/accordion/accordion.component';
+import { NavigationComponent } from '../shared/ui/navigation/navigation.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, NavigationLinksComponent, AccordionComponent],
+  imports: [
+    CommonModule,
+    NavigationLinksComponent,
+    AccordionComponent,
+    NavigationComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
