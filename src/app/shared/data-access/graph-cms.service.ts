4d09d405e0ff207f745fba8022e38636f61ef3ef
WojtekP <wojtekpietraszuk@gmail.com>
2023-06-09 12:16:52 +0200

    Data depends on the configuration type
    In development mode the app is using mock data and for production app is using data from graph cms

--- src/app/shared/data-access/graph-cms.service.ts
+++ src/app/shared/data-access/graph-cms.service.ts
@@ -4,33 +4,38 @@ import { from, map, of } from 'rxjs';
 import { Record } from 'src/app/shared/models/record.model';
 import { getQuestionsAndAnswers } from '../query/questionsAndAnswers.query';
 import { getArticles } from '../query/articles.query';
-import { environment } from '../../../environments/environment.development';
 import { Article } from 'src/app/shared/models/article.model';
 import { questionsAndAnswers } from '../mock-data/questionsAndAnswers';
 import { articles } from '../mock-data/articles';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RecordsService {
   getQuestionsAndAnswers() {
-    // return of(questionsAndAnswers);
-    return from(
-      // questionsAndAnswers
-      request<{ records: Record[] }>(
-        environment.graphCmsEndpoint,
-        getQuestionsAndAnswers()
-      )
-    ).pipe(map(data => data.records));
+    if (environment.production) {
+      return from(
+        request<{ records: Record[] }>(
+          environment.graphCmsEndpoint,
+          getQuestionsAndAnswers()
+        )
+      ).pipe(map(data => data.records));
+    } else {
+      return of(questionsAndAnswers);
+    }
   }
 
   getArticles() {
-    // return of(articles);
-    return from(
-      request<{ articles: Article[] }>(
-        environment.graphCmsEndpoint,
-        getArticles()
-      )
-    ).pipe(map(data => data.articles));
+    if (environment.production) {
+      return from(
+        request<{ articles: Article[] }>(
+          environment.graphCmsEndpoint,
+          getArticles()
+        )
+      ).pipe(map(data => data.articles));
+    } else {
+      return of(articles);
+    }
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
+  production: true,
   graphCmsEndpoint:
     'https://eu-central-1-shared-euc1-02.cdn.hygraph.com/content/clf4danvs4n6h01uj5aj8c9pp/master',
 };
